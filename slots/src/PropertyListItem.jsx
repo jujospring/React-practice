2a5c6ba0f9ba82854706f48f75cca288b2ae1b83
jujospring <jujospring@gmail.com>
2024-05-21 15:38:58 -0400

    Fixing bad repo structure

--- slots/src/PropertyListItem.jsx
+++
@@ -1,9 +0,0 @@
-export default function PropertyListItem({name, rating, price}) {
-    return (
-        <div className="PropertyListItem">
-            <h2>{name}</h2>
-            <h3>${price} per night</h3>
-            <h4>{rating}/5.0</h4>
-        </div>
-    );
-}
